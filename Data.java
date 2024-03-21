5dc65ec1777ae24c6c8a443a8b66357984d6d360
unknown <asmte2780@gmail.com>
2024-03-21 17:16:03 +0300

    Update after 2 years

--- Data.java
+++ Data.java
@@ -1,29 +1,27 @@
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.math.BigInteger;
 import java.util.Scanner;
 
-class Data {
+public class Data {
     static Scanner scan = new Scanner(System.in);
     static Scanner scan1 = new Scanner(System.in);
 
     static int Size;
     static String Path;
+    static final int Giga = 134217728;
     public static void main(String[] args) {
-
-        
         inputs();
         createfile(Pathconvert(Path));
         writedata(Size,Path);
         System.out.println("Done");
     }
+    
 
     static String Pathconvert(String Path){
         String newstr = "";
         String[] PathArray = new String[Path.length() * 2];
         for (int i = 0; i < Path.length(); i++) {
-            if(Path.charAt(i ) == '\\'){
+            if(Path.charAt(i) == '\\'){
                 PathArray[i] = "\\" + "\\";
             }
             else {
@@ -39,17 +37,21 @@ class Data {
         }
         return newstr;
     }
+
     static void inputs() {
         System.out.println("Input your Data Size (GB)");
         Size = scan.nextInt();
 
         System.out.println("Select the Path of data : ");
         Path = scan1.nextLine();
+        if(Path == ""){
+            Path = System.getProperty("user.dir");
+        }
     }
 
     static void createfile(String Path) {
         try {
-            File myObj = new File(Path + "\\Data.txt");
+            new File(Path + "\\Data.txt");
 
         } catch (Exception e) {
             System.out.println("Error on createing file");
@@ -57,7 +59,7 @@ class Data {
     }
 
     static void writedata(long Data_Size, String Path) {
-        int Giga = 134217728;
+        
 
         try {
             FileWriter myobj = new FileWriter(Path + "\\Data.txt");
@@ -69,7 +71,7 @@ class Data {
 
             myobj.close();
         } catch (Exception e) {
-
+            System.out.println("Error writing data: " + e.getMessage());
         }
 
     }
